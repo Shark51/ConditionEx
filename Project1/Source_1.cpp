8e9c7e90553302a3483183f00d80d6dcb3467225
Shark51 <dlfrnjs51@naver.com>
2022-01-07 11:24:15 +0900

    second commit

--- Project1/Source_1.cpp
+++ Project1/Source_1.cpp
@@ -1,11 +1,28 @@
 #include<iostream>
 
 int main() {
-	int sum=0;
-	for (int i=0; i <= 10; i++) {
-		sum += i;
+	/*
+	for (int j=1; j < 10; j++) {
+		for (int i = 2; i < 10; i++) {
+			std::cout << i; std::cout << "*"; std::cout << j; std::cout << " = ";
+			std::cout << i * j;  std::cout << " ";
+
+
+
+		}
+		std::cout<< std::endl;
 	}
-	std::cout <<"гую╨ : " << sum << std::endl;
+	
+	int i = 0, sum = 0;
+	while ( i <= 10) {	
+		sum += i;
+		i++;
+
+	}
+	std::cout << sum;
+	*/
+
+
 
 	return 0;
 
